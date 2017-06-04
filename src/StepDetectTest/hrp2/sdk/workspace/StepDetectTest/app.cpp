ba432322fa01900af9ed67c5b387ccd1201db3e7
Yoshimasa Nagasaka <yoshimasa.nagasaka@gmail.com>
2017-06-04 16:56:17 +0900

    [src]階段検知テストプログラム
     * 階段にタイヤがひっかかると停止する

--- src/StepDetectTest/hrp2/sdk/workspace/StepDetectTest/app.cpp
+++ src/StepDetectTest/hrp2/sdk/workspace/StepDetectTest/app.cpp
@@ -20,39 +20,44 @@
 
 using namespace ev3api;
 
-#define DEBUG
+/* 標準define */
+#define TRUE  1
+#define FALSE 0
 
-#ifdef DEBUG
-#define _debug(x) (x)
-#else
-#define _debug(x)
-#endif
+/* 障害物define */
+#define SONER_SOMETHINGTHERE  1
 
-/* Bluetooth */
-static int32_t   bt_cmd = 0;      /* Bluetoothコマンド 1:リモートスタート */
-static FILE     *bt = NULL;      /* Bluetoothファイルハンドル */
+/* 1: Bluetoothスタートを有効にする */
+#define BT_ENABLE 0
 
 /* 下記のマクロは個体/環境に合わせて変更する必要があります */
 #define GYRO_OFFSET           0  /* ジャイロセンサオフセット値(角速度0[deg/sec]時) */
-#define LIGHT_WHITE          40  /* 白色の光センサ値 */
+#define LIGHT_WHITE          20  /* 白色の光センサ値 */
 #define LIGHT_BLACK           0  /* 黒色の光センサ値 */
 #define SONAR_ALERT_DISTANCE 30  /* 超音波センサによる障害物検知距離[cm] */
 #define TAIL_ANGLE_STAND_UP  93  /* 完全停止時の角度[度] */
 #define TAIL_ANGLE_DRIVE      3  /* バランス走行時の角度[度] */
 #define P_GAIN             2.5F  /* 完全停止用モータ制御比例係数 */
 #define PWM_ABS_MAX          60  /* 完全停止用モータ制御PWM絶対最大値 */
-//#define DEVICE_NAME     "ET0"  /* Bluetooth名 hrp2/target/ev3.h BLUETOOTH_LOCAL_NAMEで設定 */
-//#define PASS_KEY        "1234" /* パスキー    hrp2/target/ev3.h BLUETOOTH_PIN_CODEで設定 */
-#define CMD_START         '1'    /* リモートスタートコマンド */
+#define CMD_START           '1'  /* リモートスタートコマンド */
+#define DETECTSTEP_VEL        5  /* 階段に衝突したときに検知する角速度 */
 
 /* LCDフォントサイズ */
 #define CALIB_FONT (EV3_FONT_SMALL)
 #define CALIB_FONT_WIDTH (6/*TODO: magic number*/)
 #define CALIB_FONT_HEIGHT (8/*TODO: magic number*/)
 
+/* 関数内変数 */
+#if BT_ENABLE == 1
+static int32_t   bt_cmd = 0;     /* Bluetoothコマンド 1:リモートスタート */
+static FILE     *bt = NULL;      /* Bluetoothファイルハンドル */
+#endif
+
 /* 関数プロトタイプ宣言 */
 static int32_t sonar_alert(void);
 static void tail_control(int32_t angle);
+static void tail_control_slowly(int32_t angle);
+
 
 /* オブジェクトへのポインタ定義 */
 TouchSensor*    touchSensor;
@@ -70,6 +75,9 @@ void main_task(intptr_t unused)
     int8_t forward;      /* 前後進命令 */
     int8_t turn;         /* 旋回命令 */
     int8_t pwm_L, pwm_R; /* 左右モータPWM出力 */
+    int32_t motor_ang_l, motor_ang_r;
+    int32_t volt;
+    int16_t gyro;
 
     /* 各オブジェクトを生成・初期化する */
     touchSensor = new TouchSensor(PORT_1);
@@ -85,35 +93,38 @@ void main_task(intptr_t unused)
     ev3_lcd_fill_rect(0, 0, EV3_LCD_WIDTH, EV3_LCD_HEIGHT, EV3_LCD_WHITE);
     ev3_lcd_draw_string("EV3way-ET sample_cpp", 0, CALIB_FONT_HEIGHT*1);
 
-    /* 尻尾モーターのリセット */
-    tailMotor->reset();
-
+#if BT_ENABLE == 1
     /* Open Bluetooth file */
     bt = ev3_serial_open_file(EV3_SERIAL_BT);
     assert(bt != NULL);
 
     /* Bluetooth通信タスクの起動 */
     act_tsk(BT_TASK);
+#endif
 
-    ev3_led_set_color(LED_ORANGE); /* 初期化完了通知 */
+
+    /* 尻尾モーターのリセット */
+    tailMotor->reset();
+    /* 初期化完了通知 */
+    ev3_led_set_color(LED_ORANGE);
 
     /* スタート待機 */
     while(1)
     {
-        tail_control(TAIL_ANGLE_STAND_UP); /* 完全停止用角度に制御 */
-
-        if (bt_cmd == 1)
-        {
-            break; /* リモートスタート */
-        }
-
-        if (touchSensor->isPressed())
-        {
-            break; /* タッチセンサが押された */
-        }
-
+        /* 完全停止用角度に制御 */
+        tail_control(TAIL_ANGLE_STAND_UP);
+        /* 10ms wait */
         clock->sleep(10);
+
+        if(touchSensor->isPressed())
+        {
+            /* タッチセンサが押された */
+            break;
+        }
     }
+
+    /* スタート通知 */
+    ev3_led_set_color(LED_GREEN);
 
     /* 走行モーターエンコーダーリセット */
     leftMotor->reset();
@@ -121,28 +132,29 @@ void main_task(intptr_t unused)
 
     /* ジャイロセンサーリセット */
     gyroSensor->reset();
-    balance_init(); /* 倒立振子API初期化 */
+    /* 倒立振子API初期化 */
+    balance_init();
 
-    ev3_led_set_color(LED_GREEN); /* スタート通知 */
-
-    /**
-    * Main loop for the self-balance control algorithm
-    */
     while(1)
     {
-        int32_t motor_ang_l, motor_ang_r;
-        int32_t gyro, volt;
 
-        if (ev3_button_is_pressed(BACK_BUTTON)) break;
+        /* 中央ボタンを押すと終了 */
+        if (ev3_button_is_pressed(ENTER_BUTTON)) break;
 
-        tail_control(TAIL_ANGLE_DRIVE); /* バランス走行用角度に制御 */
-
-        if (sonar_alert() == 1) /* 障害物検知 */
+        /* バランス走行用角度に制御 */
+        tail_control_slowly(TAIL_ANGLE_DRIVE);
+#if 0
+        /* 障害物検知は一度削除 */
+        if (sonar_alert() == SONER_SOMETHINGTHERE)
         {
-            forward = turn = 0; /* 障害物を検知したら停止 */
+            /* 障害物を検知したら停止 */
+            forward = 0;
+            turn = 0;
         }
         else
+#endif
         {
+            /************ ライントレース ************/
             forward = 30; /* 前進命令 */
             if (colorSensor->getBrightness() >= (LIGHT_WHITE + LIGHT_BLACK)/2)
             {
@@ -151,6 +163,17 @@ void main_task(intptr_t unused)
             else
             {
                 turn = -20; /* 右旋回命令 */
+            }
+
+            /************ ジャイロセンサテスト ************/
+            /* 現在、ジャイロセンサに掛かっている角速度[deg/sec]を取得 */
+            gyro = gyroSensor->getAnglerVelocity();
+            if((gyro > DETECTSTEP_VEL) || (gyro < -DETECTSTEP_VEL))
+            {
+                /* ロボットがDETECTSTEP_VEL以上振動した */
+                /* 階段を検知したので、停止する */
+                forward = 0;
+                turn = 0;
             }
         }
 
@@ -181,8 +204,10 @@ void main_task(intptr_t unused)
     leftMotor->reset();
     rightMotor->reset();
 
+#if BT_ENABLE == 1
     ter_tsk(BT_TASK);
     fclose(bt);
+#endif
 
     ext_tsk();
 }
@@ -241,7 +266,38 @@ static void tail_control(int32_t angle)
         pwm = -PWM_ABS_MAX;
     }
 
-    tailMotor->setPWM(pwm);
+    tailMotor->setPWM((int)pwm);
+}
+
+//*****************************************************************************
+// 関数名 : tail_control_slowly
+// 引数 : angle (モータ目標角度[度])
+// 返り値 : 無し
+// 概要 : ゆっくりモータの角度制御
+//*****************************************************************************
+static void tail_control_slowly(int32_t angle)
+{
+    float pwm;
+    if(tailMotor->getCount() > 10)
+    {
+        /* 角度が10度以上離れているときは、ゆっくり一定速度で戻す */
+        pwm = 15.0F;
+    }
+    else
+    {
+        pwm = (float)(angle - tailMotor->getCount()) * P_GAIN; /* 比例制御 */
+    }
+    /* PWM出力飽和処理 */
+    if (pwm > PWM_ABS_MAX)
+    {
+        pwm = PWM_ABS_MAX;
+    }
+    else if (pwm < -PWM_ABS_MAX)
+    {
+        pwm = -PWM_ABS_MAX;
+    }
+
+    tailMotor->setPWM((int)pwm);
 }
 
 //*****************************************************************************
@@ -253,6 +309,7 @@ static void tail_control(int32_t angle)
 //*****************************************************************************
 void bt_task(intptr_t unused)
 {
+#if BT_ENABLE == 1
     while(1)
     {
         uint8_t c = fgetc(bt); /* 受信 */
@@ -266,4 +323,5 @@ void bt_task(intptr_t unused)
         }
         fputc(c, bt); /* エコーバック */
     }
+#endif
 }
